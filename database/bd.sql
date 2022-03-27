bf5bc7a3c52af76e2d9ebf90d43f0229c199efc6
Mariana Gonzalez <mariana.elizabeth.02@gmail.com>
2022-03-27 17:14:35 -0600

    Estructura principal completa y base de datos

---
+++ database/bd.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE Copro;
+
+USE Copro;
+
+CREATE TABLE Usuarios(
+    Id int PRIMARY KEY AUTO_INCREMENT,
+    Nombre varchar(100),
+    Usuario varchar(50),
+    Contrasena varchar(200)
+);
+
+CREATE TABLE Preguntas(
+    Id int PRIMARY KEY AUTO_INCREMENT,
+    Titulo varchar(100),
+    Descripcion varchar(800),
+    Imagen varchar(100),
+    Categoría enum('MySQL', 'SQL Server', 'C#', 'C++', 'Java Script', 'NodeJS'),
+    fkUsuario int,
+    CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario) REFERENCES Usuarios(Id)
+);
+
+CREATE TABLE Respuestas(
+    Id int PRIMARY KEY AUTO_INCREMENT,
+    Respuesta varchar(800),
+    Imagen varchar(100),
+    fkPregunta int,
+    fkUsuario int,
+    CONSTRAINT fkPregunta FOREIGN KEY (fkPregunta) REFERENCES Preguntas(Id),
+    CONSTRAINT fkUsu FOREIGN KEY (fkUsuario) REFERENCES Usuarios(Id)
+);
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const path = require('path');
+
+//Inicializacion
+const app = express();
+
+//Configuraciones
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//Variables globales
+app.use((req, res, next)=>
+{
+  next();
+});
+
+//Rutas
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/vistas', require('./routes/vistas'));
+
+//Publico
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Empezar el servidor
+app.listen(app.get('port'), () => {
+    console.log('Servidor en el puerto', app.get('port'));
+});
